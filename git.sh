132c50aa0df8d4f1cf2766cd37a8b90e5d8c6c0e
Dan Alvidrez <dan@bayareawebpro.com>
2019-09-24 05:16:42 -0700

    Update git.sh

--- git.sh
+++ git.sh
@@ -3,42 +3,61 @@
 # Hard Reset to HEAD
 function git:reset() {
   git reset --hard HEAD
-  echo "Repository Reverted."
+  echo "Repository Reverted!"
 }
 
 # Initialize GIT Repository
 function git:init() {
+  echo "Initializing local repository..."
   if [ -d ".git" ]; then
     echo "This directory has already been initialized with git."
     exit 1
   else
     git init
+    echo "Local Repository Created!"
   fi
   get:connect
   git:make:readMe
   git:make:ignore
-  git:make:docs
   git:make:develop
+  git:make:docs
 }
 
 # Add Remote Connection
 function get:connect() {
+  echo "Connecting to Remote Repository..."
   REPO=$(basename "$PWD")
   git remote add origin "git@github.com:bayareawebpro/${REPO}"
 }
 
+# Add Commit & Push to Remote
+function git:save() {
+  echo "Synchronizing with Remote Repository..."
+  if [ -z "$1" ]; then
+    MESSAGE="WIP"
+  else
+    MESSAGE="$1"
+  fi
+  echo "${MESSAGE}"
+  git add . && git commit -m "${MESSAGE}" && git push
+  echo "Remote Repository Synchronized!"
+}
+
 # Add ReadMe
 function git:make:readMe() {
+  echo "Making README.md..."
   if [ -f "README.md" ]; then
     echo "README.md already exists."
   else
     touch README.md
     echo "# ${REPO}" >>README.md
+    echo "README.md Created!"
   fi
 }
 
 # Add .gitignore
 function git:make:ignore() {
+  echo "Making .gitignore..."
   if [ -f ".gitignore" ]; then
     echo ".gitignore already exists."
   else
@@ -48,32 +67,30 @@ function git:make:ignore() {
 /vendor
 /node_modules
 /public/hot" >>.gitignore
+    echo ".gitignore Created!"
   fi
-}
-
-# Add Commit & Push to Remote
-function git:commitAndPush() {
-  if [ -z "$1" ]; then
-    MESSAGE="WIP"
-  else
-    MESSAGE="$1"
-  fi
-  echo "${MESSAGE}";
-  git add . && git commit -m "${MESSAGE}" && git push
 }
 
 # Add GithubPages Branch
 function git:make:docs() {
+  echo "Creating Documentation Branch..."
   git checkout --orphan "gh-pages" && git rm -rf .
   git push --set-upstream origin gh-pages
   git:make:ignore
   git:make:readMe
-  #git:commitAndPush "Initial GhPages Commit"
+  echo "Branch Created Successfully!"
+  echo "Run git:save to syncronize."
+  #git:save "Initial GhPages Commit"
 }
 
 # Add Develop Branch
 function git:make:develop() {
+  echo "Creating Development Branch..."
   git checkout -b "develop"
   git push --set-upstream origin develop
-  #git:commitAndPush "Initial Develop Commit"
+  git:make:ignore
+  git:make:readMe
+  echo "Branch Created Successfully!"
+  echo "Run git:save to syncronize."
+  #git:save "Initial Develop Commit"
 }
