fa951914bd653b1134fcf5cf13672a9382164f14
Dan Alvidrez <dan@bayareawebpro.com>
2019-10-23 00:59:30 -0700

    WIP

--- utility-chrome.sh
+++ utility-chrome.sh
@@ -3,11 +3,11 @@ function chrome:serve(){
   local DOMAIN
   if [ -n "$1" ]; then
     DOMAIN="https://$1"
-    logger:success "Launching Chrome..."
   else
     logger:input "Domain Name or IP?" "ANSWER"
     DOMAIN="https://$ANSWER"
   fi
+  logger:success "Launching Chrome..."
   /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome "$DOMAIN" --user-data-dir=/tmp/foo
   #php artisan serve
 }
@@ -16,11 +16,11 @@ function chrome:secure(){
   local DOMAIN
   if [ -n "$1" ]; then
     DOMAIN="https://$1"
-    logger:success "Launching Chrome..."
   else
     logger:input "Domain Name or IP?" "ANSWER"
     DOMAIN="https://$ANSWER"
   fi
+  logger:success "Launching Chrome Fake Secure..."
   /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome "$DOMAIN" --user-data-dir=/tmp/foo --ignore-certificate-errors --unsafely-treat-insecure-origin-as-secure="$DOMAIN:443"
   #php artisan serve
 }
