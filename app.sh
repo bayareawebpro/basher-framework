b8f6b41ca55fc926bb590fe4b19ff3624272cf90
Dan Alvidrez <dan@bayareawebpro.com>
2019-10-24 09:28:20 -0700

    WIP

--- app.sh
+++ app.sh
@@ -61,7 +61,7 @@ if string:not:empty "$BASHER_BANNER"; then
   colors:green  '|_____] |_____| |______ |_____| |______ |_____/'
   colors:blue   '|_____] |     | ______| |     | |______ |    \_'
   colors:white  'Version 1.x'
-  logger:info "Basher Loaded & Ready."
+  logger:info "Loaded & Ready for Commands."
   logger:divider
 fi
 
