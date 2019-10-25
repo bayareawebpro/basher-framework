6bbafba99fbe1f64a0cc2337298af371cc45e28c
Dan Alvidrez <dan@bayareawebpro.com>
2019-10-25 06:00:18 -0700

    WIP

--- tests/remote-mock.sh
+++ tests/remote-mock.sh
@@ -4,6 +4,13 @@ echo "Args: $2 $3"
 
 if [[ -d "$BASHER_PATH" ]] && cd "$BASHER_PATH"; then
   # shellcheck source=$BASHER_PATH/app.sh
-  source "$BASHER_PATH/app.sh"
+  source "$BASHER_PATH/bootstrap/app.sh"
+  # Utilities
+  app:require:all utilities
+  app:require:all services
+  app:require:all commands
+  app:require:all installers
+  # Boot the Application
+  app:boot
   logger:success "Loaded Remote Script from Github."
 fi
