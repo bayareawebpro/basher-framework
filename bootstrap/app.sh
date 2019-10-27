da24eb30f1898d72c7453643feddc419270acf04
Dan Alvidrez <dan@bayareawebpro.com>
2019-10-26 17:27:49 -0700

    WIP

--- bootstrap/app.sh
+++ bootstrap/app.sh
@@ -53,3 +53,15 @@ function app:banner() {
     logger:divider
   fi
 }
+
+# ==== Publish Files ====
+function app:publish() {
+  if path:is:file "$2"; then
+    logger:info "$2 already exists."
+  elif file:exists "$BASHER_PATH/$1" && file:copy "$BASHER_PATH/$1" "$2"; then
+    logger:success "Published $BASHER_PATH/$1 => $2 completed."
+  else
+    logger:failed "Publishing $BASHER_PATH/$1 => $2 failed."
+    return 1
+  fi
+}
