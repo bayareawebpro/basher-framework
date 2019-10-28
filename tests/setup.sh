b4d85db8955a426f8df39289612b3b363d2ab696
Dan Alvidrez <dan@bayareawebpro.com>
2019-10-28 14:13:17 -0700

    WIP

--- tests/setup.sh
+++ tests/setup.sh
@@ -9,6 +9,7 @@ function assert:truthy() {
     else
     logger:failed "Assert: $1 true/Successful."
   fi
+  sleep 1
 }
 function assert:falsy() {
   if func:failed; then
@@ -16,6 +17,7 @@ function assert:falsy() {
     else
     logger:failed "Assert: $1 false/Failed."
   fi
+  sleep 1
 }
 
 #Test Loader
