256ae1258d4257d086e470c4b85f30ba87eb0efb
Dan Alvidrez <dan@bayareawebpro.com>
2019-10-28 14:23:06 -0700

    WIP

--- tests/unit/5-process-test.sh
+++ tests/unit/5-process-test.sh
@@ -2,14 +2,6 @@
 MOCKED="$BASHER_PATH/tests/mocks/process-mock.sh"
 echo "" > "$MOCKED.log"
 
-process:running "$MOCKED"
-assert:falsy "process:running"
-sleep 1
-
-process:status "$MOCKED"
-assert:falsy "process:status"
-sleep 1
-
 process:start "$MOCKED"
 assert:truthy "process:start"
 sleep 1
@@ -32,7 +24,3 @@ sleep 1
 
 process:stop "$MOCKED"
 assert:truthy "process:stop"
-sleep 1
-
-process:status "$MOCKED"
-assert:falsy "process:status"
