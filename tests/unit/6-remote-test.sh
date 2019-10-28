c17af6e9927959ccee2bbe14f74bc756e526c7c0
Dan Alvidrez <dan@bayareawebpro.com>
2019-10-28 14:24:50 -0700

    WIP

--- tests/unit/6-remote-test.sh
+++ tests/unit/6-remote-test.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 SCRIPT="https://raw.githubusercontent.com/bayareawebpro/basher-framework/master/tests/mocks/remote-mock.sh"
 
-remote:ping "https://raw.githubusercontent.com"
-assert:truthy "remote:ping"
-
 remote:require "$SCRIPT" myVar
 assert:truthy "remote:require"
+
+if str:filled "$BASHER_CI"; then
+  logger:warning "Skipping, no ping available..."
+  continue
+fi
+
+remote:ping "https://raw.githubusercontent.com"
+assert:truthy "remote:ping"
